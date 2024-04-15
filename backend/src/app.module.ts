5acc7ca7852cf0713671735e27ea330134b83878
Gwanho Kim <khkim6040@postech.ac.kr>
2024-04-15 22:28:44 +0900

    Add GraphQL dependencies and implement TokenService

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -7,32 +7,61 @@ import { GraphQLModule } from '@nestjs/graphql';
 import { ApolloDriver } from '@nestjs/apollo';
 import { join } from 'path';
 import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TokenService } from './token/token.service';
 
-
-
+import { RedisPubSub } from 'graphql-redis-subscriptions';
+const pubSub = new RedisPubSub({
+  connection: {
+    host: process.env.REDIS_HOST || 'localhost',
+    port: parseInt(process.env.REDIS_PORT || '6379', 10),
+    retryStrategy: (times) => {
+      // reconnect after
+      return Math.min(times * 50, 2000);
+    },
+  },
+});
 
 @Module({
-  imports: [AuthModule, UserModule,
+  imports: [
+    AuthModule,
+    UserModule,
     GraphQLModule.forRootAsync({
       imports: [ConfigModule, AuthModule],
       inject: [ConfigService],
       driver: ApolloDriver,
       useFactory: async (
         configService: ConfigService,
+        tokenService: TokenService,
       ) => {
         return {
+          installSubscriptionHandlers: true,
           playground: true,
           autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
           sortSchema: true,
-        }
-      }
-      
+          subscriptions: {
+            'graphql-ws': true,
+            'subscriptions-transport-ws': true,
+          },
+          onConnect: (connectionParams) => {
+            const token = tokenService.extractToken(connectionParams);
+
+            if (!token) {
+              throw new Error('Token not provided');
+            }
+            const user = tokenService.validateToken(token);
+            if (!user) {
+              throw new Error('Invalid token');
+            }
+            return { user };
+          },
+        };
+      },
     }),
     ConfigModule.forRoot({
       isGlobal: true,
     }),
   ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, TokenService],
 })
 export class AppModule {}
